1ee6be00da0e862145b8ccf17c7f8627e3891112
David E. Keller <david.keller@noser.com>
2025-05-14 22:28:21 +0200

    Refactor backup scripts to improve logging and error handling; initialize repository if it doesn't exist.

--- README.md
+++ README.md
@@ -27,16 +27,33 @@ REPO_PATH="ssh://user@server/mnt/backups/"
 
 export BACKUP_PATH=/mnt/user/paperless-export
 export REPO_NAME=paperless
+export REPO_PATH=/mnt/user/backups/borg/
 export LOG_PATH="/boot/logs/borg.log"
 
+touch $LOG_PATH
+
 docker run --rm  \
---env-file .env \
--e REPO_NAME="$REPO_NAME" \
--v $BACKUP_PATH:/mnt/source \
--v $LOG_PATH:/logs/log.txt \
--v /boot/config/ssh/borg_key:/root/.ssh/borg_key:ro \
--v ./create-backup.sh:/backup.sh:ro \
-alpine:latest \
-sh -c "apk add --no-cache borgbackup openssh && sh /backup.sh"
+    --env-file .env \
+    -e REPO_NAME="$REPO_NAME" \
+    -e REPO_PATH=/mnt/backupdest \
+    -v $REPO_PATH:"/mnt/backupdest" \
+    -v $BACKUP_PATH:/mnt/source \
+    -v $LOG_PATH:/logs/log.txt \
+    -v /boot/config/ssh/borg_key:/root/.ssh/borg_key:ro \
+    -v ./create-backup.sh:/backup.sh:ro \
+    alpine:latest \
+    sh -c "apk add --no-cache borgbackup openssh && sh /backup.sh"
 ```
 
+
+
+```bash
+
+export REPO_PATH=/mnt/user/backups/borg/
+
+docker run --rm  \
+    --env-file /mnt/user/borg/unraid-borg-backup/.env \
+    -v $REPO_PATH:"/mnt/backupdest" \
+    alpine:latest \
+    sh -c "apk add --no-cache borgbackup openssh && borg list /mnt/backupdest"
+```
--- create-backup.sh
+++ create-backup.sh
@@ -2,17 +2,22 @@
 
 set -u
 
-if [ -n "$REPO_PATH" ]; then
+log_message() {
+    local message="$1"
+    echo "$(date "+%m-%d-%Y %T") : $message" 2>&1 | tee -a /logs/log.txt
+}
+
+if [ -z "$REPO_PATH" ]; then
     echo "Error: The environment variable REPO_PATH is not set!"
     exit 1
 fi
-if [ -n "$REPO_NAME" ]; then
+if [ -z "$REPO_NAME" ]; then
     echo "Error: The environment variable REPO_NAME is not set!"
     exit 1
 fi
 
-if [ -n "$REPO_PASS" ]; then
-    echo "Error: The environment variable REPO_PASS is not set!"
+if [ -z "$BORG_PASSPHRASE" ]; then
+    echo "Error: The environment variable BORG_PASSPHRASE is not set!"
     exit 1
 fi
 
@@ -20,12 +25,26 @@ fi
 LOGFILE=/logs/log.txt
 SSH_KEY="/root/.ssh/borg_key" # Pfad zum SSH-Key im Container
 
-export BORG_PASSPHRASE=$REPO_PASS
 
 # Set SSH command to use the specific key
 export BORG_RSH="ssh -i $SSH_KEY -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 
-echo "$(date "+%m-%d-%Y %T") : Borg backup has started" 2>&1 | tee -a $LOGFILE
+
+# Check if repository exists, initialize if not
+if ! borg info "$REPO_PATH" > /dev/null 2>&1; then
+  log_message "Repository does not exist. Initializing repository..."
+  borg init --encryption=repokey "$REPO_PATH"
+  INIT_EXIT_CODE=$?
+  if [ $INIT_EXIT_CODE -ne 0 ]; then
+    log_message "ERROR: Failed to initialize repository. Exit code $INIT_EXIT_CODE."
+    exit $INIT_EXIT_CODE
+  fi
+else
+  log_message "Repository exists."
+fi
+
+
+log_message "Borg backup has started" 
 
 # Create a backup
 borg create \
@@ -39,8 +58,12 @@ borg create \
     --exclude-caches                \
     --progress $REPO_PATH::"$REPO_NAME-{now:%Y-%m-%d_%H-%M-%S}" /mnt/source
 
-# Prune old backups (optional)
+log_message "Borg backup has finished, pruning old backups..."
+log_message "Pruning: Keeping 7 daily, 4 weekly, and 6 monthly backups."
+
 borg prune --keep-daily=7 --keep-weekly=4 --keep-monthly=6 $REPO_PATH
+
+log_message "Pruning completed. Script finished successfully."
 
 # Unset the passphrase
 unset BORG_PASSPHRASE
