8d31c6db47c7ddcc57714a8729016a65ab7c2d02
David E. Keller <david.keller@dkbe.ch>
2025-05-17 21:58:14 +0200

    feat: add backup script for Paperless remote backups

---
+++ backup-paperless-remote.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export BACKUP_PATH=/mnt/user/paperless-export
+export REPO_NAME=paperless
+export LOG_PATH="/boot/logs/borg-paperless-remote.log"
+
+export BORG_CACHE_DIR='/mnt/user/appdata/borg/cache/'
+
+touch $LOG_PATH
+
+docker run --rm  \
+    --env-file .env \
+    -e REPO_NAME="$REPO_NAME" \
+    -v /root/.ssh/:/ssh/ \
+    -v $BORG_CACHE_DIR:/mnt/borg/cache \
+    -v $BACKUP_PATH:/mnt/source \
+    -v $LOG_PATH:/logs/log.txt \
+    -v ./create-backup.sh:/backup.sh:ro \
+    alpine:latest \
+    sh -c "apk add --no-cache borgbackup openssh && sh /backup.sh"
+
