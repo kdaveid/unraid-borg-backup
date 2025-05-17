25dc36be74a4297c6f4b53954500569817f2a01c
David E. Keller <david.keller@dkbe.ch>
2025-05-17 22:47:40 +0200

    feat: add backup script for local photos to external disk

---
+++ backup-photos-local-ext-disk.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd /mnt/user/borg/unraid-borg-backup
+
+export BACKUP_PATH=/mnt/user/photos/immich/
+export REPO_NAME=photos
+export REPO_PATH=/mnt/disks/WCK5DGVZ/borg-backup/
+export LOG_PATH="/boot/logs/borg-photos-lcl-ext-disk.log"
+
+touch $LOG_PATH
+
+docker run --rm  \
+    --name borg-photos-lcl-ext-disk \
+    --env-file .env \
+    -e REPO_NAME="$REPO_NAME" \
+    -e REPO_PATH=/mnt/backupdest \
+    -v $REPO_PATH:"/mnt/backupdest" \
+    -v $BACKUP_PATH:/mnt/source \
+    -v $LOG_PATH:/logs/log.txt \
+    -v ./create-backup.sh:/backup.sh:ro \
+    alpine:latest \
+    sh -c "apk add --no-cache borgbackup openssh && sh /backup.sh"
