48664aca35473e482ea647cca0d6bc7e45382404
David E. Keller <david.keller@dkbe.ch>
2025-05-17 22:23:46 +0200

    feat: update backup script for local photos and add remote backup script

--- backup-photos-remote.sh
+++ backup-photos-remote.sh
@@ -2,18 +2,20 @@
 
 export BACKUP_PATH=/mnt/user/photos/immich/
 export REPO_NAME=photos
-export REPO_PATH=/mnt/user/backups/borg/
-export LOG_PATH="/boot/logs/borg-photos-lcl.log"
+export LOG_PATH="/boot/logs/borg-photos-remote.log"
+
+export BORG_CACHE_DIR='/mnt/user/appdata/borg/cache/'
 
 touch $LOG_PATH
 
 docker run --rm  \
     --env-file .env \
     -e REPO_NAME="$REPO_NAME" \
-    -e REPO_PATH=/mnt/backupdest \
-    -v $REPO_PATH:"/mnt/backupdest" \
+    -v /root/.ssh/:/ssh/ \
+    -v $BORG_CACHE_DIR:/mnt/borg/cache \
     -v $BACKUP_PATH:/mnt/source \
     -v $LOG_PATH:/logs/log.txt \
     -v ./create-backup.sh:/backup.sh:ro \
     alpine:latest \
     sh -c "apk add --no-cache borgbackup openssh && sh /backup.sh"
+
