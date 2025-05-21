f26ec874313bf2b426ebd417e634da55dc9086d3
David E. Keller <david.keller@dkbe.ch>
2025-05-21 22:55:57 +0200

    feat: enhance local-paperless-export script with detailed notification handling and success messages

--- others/local-paperless-export.sh
+++ others/local-paperless-export.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+export NOTIFY_PATH=/usr/local/emhttp/webGui/scripts/notify 
+
 docker exec paperless-ngx document_exporter ../export -p 
-/usr/local/emhttp/webGui/scripts/notify -i normal -s "Borg Paperless Export Run" -d " Paperless Export at `date`"
+
+EXPORT_EXIT_CODE=$?
+
+if [ $EXPORT_EXIT_CODE -eq 0 ]; then
+    $NOTIFY_PATH -e PaperlessExport -s "Rocket PaperlessExport succeeded" -d "Success" -i normal
+elif [ $EXPORT_EXIT_CODE -eq 1 ]; then
+    $NOTIFY_PATH -e PaperlessExport -s "Rocket PaperlessExport warnings" -d "Completed with warnings, exit code: $EXPORT_EXIT_CODE" -i warning
+else
+    $NOTIFY_PATH -e PaperlessExport -s "Rocket PaperlessExport FAILED" -d "Export failed! Exit Code: $EXPORT_EXIT_CODE" -i warning
+    exit $EXPORT_EXIT_CODE
+fi
+
